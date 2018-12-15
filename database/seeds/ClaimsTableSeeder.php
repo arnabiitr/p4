5e744db2bf5ac4ad5cc2c91f2108dd8875b2f024
Arnab Bhar <arnab.bhar@hotmail.com>
2018-12-15 01:05:45 -0500

    Changes

--- database/seeds/ClaimsTableSeeder.php
+++ database/seeds/ClaimsTableSeeder.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Seeder;
 use App\Claim;
+use App\Member;
 
 class ClaimsTableSeeder extends Seeder
 {
@@ -14,11 +15,11 @@ class ClaimsTableSeeder extends Seeder
     {
         # Array of claim data to add
         $claims = [
-            [1, 'ABC', 5000, 300,0],
-            [1, 'CD', 5001, 301,0],
-            [2, 'EF', 5001, 301,1],
-            [2, 'GH', 5001, 301,2],
-            [2, 'AD', 5001, 301,3]
+            ['Arit', 'ABC', 5000, 300,0],
+            ['Arit', 'CD', 5001, 301,0],
+            ['Arnab', 'EF', 5001, 301,1],
+            ['Arnab', 'GH', 5001, 301,2],
+            ['Arnab', 'AD', 5001, 301,3]
         ];
         $count = count($claims);
 
@@ -26,9 +27,11 @@ class ClaimsTableSeeder extends Seeder
         foreach ($claims as $claimData) {
             $claim = new Claim();
 
+            $member = Member::where('first_name', 'like', $claimData[0])->first();
+
             $claim->created_at = Carbon\Carbon::now()->subDays($count)->toDateTimeString();
             $claim->updated_at = Carbon\Carbon::now()->subDays($count)->toDateTimeString();
-            $claim->member_id = $claimData[0];
+            $claim->member_id = $member->id;
             $claim->diagnosis_code = $claimData[1];
             $claim->total_amount = $claimData[2];
             $claim->amount_paid = $claimData[3];
