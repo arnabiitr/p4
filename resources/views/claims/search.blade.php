1d1366189c7d47aa82294a75a3ae85155ef9f29d
Bhar, Arnab <Arnab.Bhar@ext.us.panasonic.com>
2018-12-17 08:43:20 -0500

    Cleaned up Claims Search

--- resources/views/claims/search.blade.php
+++ resources/views/claims/search.blade.php
@@ -11,7 +11,7 @@
     <form method='GET' action='/claims/search-process'>
 
         <fieldset>
-            <label for='searchTerm'>Search by Diagnosis Code:</label>
+            <label for='searchTerm'>Search by Diagnosis Code:(e.g AR,BR,ER)</label>
             <input type='text' name='searchTerm' id='searchTerm' value='{{ $searchTerm }}'>
 
             <input type='checkbox' name='caseSensitive' {{ ($caseSensitive) ? 'checked' : '' }}>
@@ -23,14 +23,28 @@
     </form>
 
     @if($searchTerm)
-        <h2>Results for query: <em>{{ $searchTerm }}</em></h2>
+        <h2><strong>Claim Details for Search with Diagnosis Code:</strong><em>{{ $searchTerm }}</em></h2>
+
 
         @if(count($searchResults) == 0)
             No matches found.
         @else
             @foreach($searchResults as $title => $claim)
                 <div class='member'>
-                    <p>Claim Details {{ $claim['diagnosis_code'] }}  ({{ $claim['total_amount']}})</p>
+
+                    <p><strong>Claim Id:</strong> {{ $claim['id']}} </p>
+                    <p><strong>Claim Amount:</strong> $ {{ $claim['total_amount']}} </p>
+                    <p><strong>Amount Paid:</strong> $ {{ $claim['amount_paid']}} </p>
+                    <p><strong>Member Name:</strong> {{$claim->members->first_name}} {{$claim->members->last_name}} With <strong>Member ID</strong> :{{$claim->members->id}}</p>
+
+                    @if ($claim['status']==2)
+                        <p><strong>Claim Status: Paid </strong></p>
+                        @elseif($claim['status']==1)
+                        <p><strong>Claim Status: In Progress</strong> </p>
+                        @elseif($claim['status']==0)
+                        <p><strong>Claim Status: Closed </strong></p>
+                    @endif
+
 
                 </div>
             @endforeach
