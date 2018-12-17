ce297ab2aff7acd2b7baf4482766ca86bdaae887
Andrew Kurtser <andrew.kurtser@ext.us.panasonic.com>
2018-12-17 15:49:58 -0500

    CSS Changes

--- resources/views/members/_member.blade.php
+++ resources/views/members/_member.blade.php
@@ -1,6 +1,26 @@
 <div class='member cf'>
     <a href='/members/{{ $member->id }}'><h3>{{ $member->first_name }}</h3></a>
-    <ul>
+
+    <table class="members-table">
+        <tr><th class="td150">Address</th>
+            <th>Insurance ID</th>
+            <th>Insurance Expiration</th>
+            <th>D.O.B.</th>
+            <th>SSN</th>
+            <th>Claims</th>
+        </tr>
+        <tr>
+            <td>{{ $member->address}}</td>
+            <td>{{ $member->insurance_id}}</td>
+            <td>{{ $member->insurance_expiration_date}}</td>
+            <td>{{ $member->dob}}</td>
+            <td>{{ $member->ssn}}</td>
+            <td>        @foreach (($member->claim) as $claimobject)
+                    <li>Claim #{{ $claimobject->id}}</li>
+                @endforeach</td>
+        </tr>
+    </table>
+<!-- <ul>
 
 
 
@@ -13,9 +33,7 @@
 
         <strong> <li>Claims:</li></strong>
 
-        @foreach (($member->claim) as $claimobject)
-            <li>Claim #{{ $claimobject->id}}</li>
-        @endforeach
 
-    </ul>
+
+    </ul>-->
 </div>
