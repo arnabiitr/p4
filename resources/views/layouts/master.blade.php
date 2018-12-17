3ea63348fd13da0221af8fc02df06ef8f109500a
Bhar, Arnab <Arnab.Bhar@ext.us.panasonic.com>
2018-12-17 16:10:24 -0500

    Revert "Bold header"
    This reverts commit 7722321a978d46f8e39007f0dbb90283ea1cd95c.

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -22,7 +22,6 @@
 
 <header>
     <a href='/'><img src='/images/sprite.png'  height="40"  border="0" id='logo' alt='p4 Logo'></a>
-    <strong>Claim Management System</strong>
     @include('modules.nav')
 </header>
 
